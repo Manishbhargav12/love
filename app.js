39d116f1a91a020512c6ff7e5153939d0db999ff
Manishbhargav12 <bhargavmanish592@gmail.com>
2026-06-13 17:12:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,19 +37,19 @@ buttons[3].addEventListener('click', () => {
   }
 });
 
-buttons[4].addEventListener('click', () => {
-    document.querySelector('#video-container').style.zIndex = '9999';
-    document.querySelector('#video-container').style.display = 'block';
-    // document.body.style.overflow = 'hidden';
+// buttons[4].addEventListener('click', () => {
+//     document.querySelector('#video-container').style.zIndex = '9999';
+//     document.querySelector('#video-container').style.display = 'block';
+//     // document.body.style.overflow = 'hidden';
   
-});
-video = document.querySelector("#my-video")
-closeVideoBtn = document.querySelector('#close-video');
-closeVideoBtn.addEventListener('click', function() {
-       document.querySelector('#video-container').style.zIndex = '-1';
-    document.querySelector('#video-container').style.display = 'none';
-    video.currentTime = 0; 
-})
+// });
+// video = document.querySelector("#my-video")
+// closeVideoBtn = document.querySelector('#close-video');
+// closeVideoBtn.addEventListener('click', function() {
+//        document.querySelector('#video-container').style.zIndex = '-1';
+//     document.querySelector('#video-container').style.display = 'none';
+//     video.currentTime = 0; 
+// })
 
 
 // buttons[4].addEventListener('click', () => {
